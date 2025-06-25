f38aa3c9116767a57f95f93f3023b387653f053d
Mon <monica.velasco.6121@gmail.com>
2025-06-25 11:13:47 -0600

    Initial commit

---
+++ src/main/java/com/mycompany/foodtrack/logic/FoodTrack.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.foodtrack.logic;
+
+/**
+ *
+ * @author monyv
+ */
+public class FoodTrack {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
